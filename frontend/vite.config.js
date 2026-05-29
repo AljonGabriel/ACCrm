9dadab08fe4d639d5145a33f4a6dc85009e1d143
Aljon Gabriel Ambas Valdez <aljongabrielambasvaldez@gmail.com>
2026-05-30 00:01:34 +0800

    Configuring render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,5 +7,8 @@ export default defineConfig({
   server: {
     host: "0.0.0.0", // or '127.0.0.1'
     port: 5173,
+    allowedHosts: [
+      "station-frontend-194g.onrender.com", // <-- add your Render frontend host
+    ],
   },
 });
